b32ddc440ea660dc1a1ccc336d50d7fe7fe7dabe
Joaquin I. Corradi <corradijoaquin@gmail.com>
2023-05-18 14:46:32 -0300

    Creando estructura del programa

---
+++ src/cliente/cliente.h
@@ -0,0 +1,25 @@
+#include <string>
+
+class cliente {
+private:
+  int numeroDeCliente;
+  std::string nombreDelCliente;
+  std::string apellidoDelCliente;
+  int anioDeIngresoDelCliente;
+  bool estadoDelCliente;
+
+public:
+  cliente(std::string, std::string);
+
+  int getNumeroDeCliente();
+  std::string getNombreDelCliente;
+  std::string getApellidoDelCliente;
+  int getAnioDeIngresoDelCliente;
+  bool getEstadoDelCliente;
+
+  void setNumeroDeCliente(int);
+  void setNombreDelCliente(std::string);
+  void setApellidoDelCliente(std::string);
+  void setAnioDeIngresoDelCliente(int);
+  void setEstadoDelCliente(bool);
+};
